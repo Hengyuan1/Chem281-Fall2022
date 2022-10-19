0ee39429e02eaeec909de039b6b66137ce442f72
Hengyuan1 <1940785820@qq.com>
2022-10-18 21:04:51 -0700

    HW3

---
+++ HW3/hw3.1_impl.cpp
@@ -0,0 +1,24 @@
+#include <hw3.1.h>
+#include <MyPotential.h>
+#include <MyCosine.h>
+
+std::complex<double> MyPotential::eval(const std::complex<double> r)
+{
+  /* implement */
+}
+
+std::complex<double> MyCosine::eval(const std::complex<double> r)
+{
+  /* implement */
+}
+
+
+/********************************/
+#include <stdio.h>
+#include <float.h>
+#include <hw3.1.h>
+
+double firstDerivative(Derivable& d, const double r)
+{
+  /* implement */
+}
